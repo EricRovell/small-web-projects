eba1d6a5b3cde8bfeef533c1d63be5713ed1fafe
Eric Rovell <ericrovell@gmail.com>
2020-05-09 23:10:42 +0300

    refactored the code

--- the-matrix/script.js
+++ the-matrix/script.js
@@ -3,7 +3,7 @@ const params = {
   bgColor: "rgba(0, 0, 0, 0.05)",
   textColor: "#0F0",
   interval: 70,
-  minHeight: 50,
+  charsHeight: [ 50, 10000 ],
   chars: [
     [ 48, 57 ],     // digits
     [ 65, 90 ],     // capital letters
@@ -19,43 +19,31 @@ const height = (context.canvas.height = window.screen.height);
 // screen's width is broken up to columns according to the size
 const columns = new Array(Math.floor(width / params.size) + 1).fill(0);
 
-function randomFromRanges() {
-  let total = 0;
-  for (const [ a, b ] of arguments) {
-    // adding the ranges' size
-    total += b - a + 1;
-  }
-  
+function randomUnicodeChar() {
+  // calc the merged range magnitude
+  const total = Array.from(arguments)
+    .reduce((acc, [ min, max ]) => acc + max - min + 1, 0);
+  // generate random int in [ 0, total ]
   let randIntInclusive = Math.floor(Math.random() * total);
-  
-  for (const [ a, b ] of arguments) {
-    if (randIntInclusive < b - a + 1) {
-      return a + randIntInclusive;
-    } else {
-      randIntInclusive -= b - a + 1;
+  // find the range this random int is located
+  for (const [ min, max ] of arguments) {
+    if (randIntInclusive < max - min + 1) {
+      return String.fromCharCode(min + randIntInclusive)
     }
+    randIntInclusive -= max - min + 1;
   }
 }
 
-const randUnicodeChar = (ranges) => (
-  String.fromCharCode(
-    //Math.floor(Math.random() * (max - min + 1)) + min
-    randomFromRanges(...ranges)
-  )
-);
-
-function render({ chars, size, bgColor, textColor, minHeight }) {
+function render({ chars, size, bgColor, textColor, charsHeight }) {
   context.fillStyle = bgColor;
   context.fillRect(0, 0, width, height);
   context.fillStyle = textColor;
-  columns.map((value, index) => {
-    // value -> y-coordinate
-    context.fillText(randUnicodeChar(chars), index * size, value);
-    columns[index] =
-      value >= height ||
-        value > minHeight + 10000 * Math.random()
-          ? 0
-          : value + size;
+  columns.forEach((value, index) => {
+    // (x, y) as (index * size, value)
+    context.fillText(randomUnicodeChar(...chars), index * size, value);
+    columns[index] = (value >= height || value > charsHeight[0] + charsHeight[1] * Math.random())
+      ? 0
+      : value + size;
   });
 }
 
